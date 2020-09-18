23db558da263e1808efea3d9748d14d4030a2e24
developeridealancy <developer.idealancy@gmail.com>
2020-09-18 11:38:23 +0500

    two forms created

---
+++ src/StepperForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form1 from './forms/Form1';
+import Form2 from './forms/Form2';
+
+const StepperForm = ()=>{
+    return(
+        <div>
+            {/* <Form1/> */}
+            <Form2/>
+        </div>
+    );
+}
+
+export default StepperForm;
